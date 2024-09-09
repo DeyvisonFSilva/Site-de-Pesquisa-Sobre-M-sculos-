5b1a3329bbeb81da9dfc50d2a62ea74a96f145e7
Deyvison Silva <178681483+DeyvisonFSilva@users.noreply.github.com>
2024-09-08 21:20:19 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {
+        titulo: "Peitoral Maior",
+        descricao: "O Peitoral maior é um músculo grande e forte localizado na parte frontal do tórax. Ele é responsável por diversos movimentos do braço, como aproximá-lo do corpo (adução), rodá-lo internamente e ajudar na flexão. Por sua localização e função, o peitoral maior é um dos músculos mais procurados por quem busca um tórax mais desenvolvido e esteticamente agradável.",
+        link: "https://pt.wikipedia.org/wiki/M%C3%BAsculo_peitoral_maior"
+       ,tags: "peitoral maior"
+    },
+
+    {
+        titulo: "Ombro",
+        descricao: "O Ombro é uma articulação complexa que permite uma ampla gama de movimentos. Ele é formado por vários ossos, músculos, ligamentos e tendões que trabalham em conjunto para proporcionar a mobilidade do braço.",
+        link: "https://pt.wikipedia.org/wiki/Ombro"
+        ,tags: "ombro"
+    },
+
+    {
+        titulo: "Tríceps",
+        descricao: "O Tríceps braquial é o músculo posterior do braço, responsável pela extensão do cotovelo. Ele é dividido em três cabeças: longa, lateral e medial.",
+        link: "https://pt.wikipedia.org/wiki/Tr%C3%ADceps_braquial"
+        ,tags: "triceps"
+    },
+
+    {
+        titulo: "Bíceps",
+        descricao: "O Bíceps braquial é o músculo anterior do braço, responsável pela flexão do cotovelo. Ele é dividido em duas cabeças: longa e curta.",
+        link: "https://pt.wikipedia.org/wiki/B%C3%ADceps_braquial"
+        ,tags: "biceps"
+    }
+];
